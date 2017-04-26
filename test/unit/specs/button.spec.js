7d1eadaa41cf2d96539ee8bd4992052857ed58da
Hejx <531601727@qq.com>
2017-04-26 13:46:58 +0800

    Fix: Button component unit test

--- test/unit/specs/button.spec.js
+++ test/unit/specs/button.spec.js
@@ -1,13 +1,48 @@
 import Button from 'src/components/css/button/button';
-import { getRenderedText } from '../utils';
+import { getRenderedText, createVue } from '../utils';
 
 describe('Button.vue', () => {
-  it('should render large and round button', () => {
+  it('should render large button', () => {
     const el = getRenderedText(Button, {
       large: true,
+    });
+    expect(el.classList.contains('v-button--large')).to.be.true;
+  });
+
+  it('should render small button', () => {
+    const el = getRenderedText(Button, {
+      small: true,
+    });
+    expect(el.classList.contains('v-button--small')).to.be.true;
+  });
+
+  it('should render round-style button', () => {
+     const el = getRenderedText(Button, {
       round: true,
     });
-    expect(el.className.indexOf('v-button--large')).to.not.equal(-1);
-    expect(el.className.indexOf('v-button--round')).to.not.equal(-1);
+    expect(el.classList.contains('v-button--round')).to.be.true;
   });
+
+  it('should render outline-style button', () => {
+     const el = getRenderedText(Button, {
+      outline: true,
+    });
+    expect(el.classList.contains('v-button--outline')).to.be.true;
+  });
+
+  it('show trigger click event', done => {
+    const vm = createVue({
+      template: '<v-button @click.native="handleClick"></v-button>',
+      components: {
+        [Button.name]: Button
+      },
+      methods: {
+        handleClick() {
+          done();
+        }
+      }
+    }, true);
+
+    vm.$el.click();
+  })
 });
